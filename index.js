cf73f7cb10f83d17db812e94080b98c232e8ffd0
ilhamrizki <irp100202@gmail.com>
2023-04-25 16:05:14 +0700

    update lagi

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const PORT = 1002;
 
 app.get('/', (request, response) => {
-	response.send(`Hello World, I'm Here Guys`);
+	console.log('get IP Local');
+	response.send(`SERVER API IS RUNNING http://192.168.0.169:${PORT} 👍`);
 });
 
 app.listen(PORT, () => console.log(`Server running on port: http://192.168.0.169:${PORT}`));
